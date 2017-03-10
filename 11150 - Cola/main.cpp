fc35f2a391aa3f21ad95af5d21db5ef4bbf0a97d
Matouš Dzivjak <matoousmaster@gmail.com>
2017-03-10 18:35:07 +0100

    Update main.cpp

--- 11150 - Cola/main.cpp
+++ 11150 - Cola/main.cpp
@@ -4,11 +4,7 @@
 int main(){
     int arr[201];
     arr[1]= 1;
-    for(int i = 2; i < 201; i++){
-        arr[i] = arr[i-1] + ((i % 2 == 1) ? 1 : 2);
-    }
-    while(std::cin>>arr[0]){
-        std::cout << arr[arr[0]] << std::endl;
-    }
+    for(int i = 2; i < 201; i++) arr[i] = arr[i-1] + ((i % 2) ? 1 : 2);
+    while(std::cin>>arr[0]) std::cout << arr[arr[0]] << std::endl;
     return 0;
 }
