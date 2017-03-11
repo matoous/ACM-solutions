3e6e36c92df0df1f9fe920322d962147a5fb5b01
Matouš Dzivjak <matoousmaster@gmail.com>
2017-03-11 22:26:40 +0100

    Update main.cpp

--- 10041-Vito's_Family/main.cpp
+++ 10041-Vito's_Family/main.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-#include "vector"
-#include "algorithm"
-
-int compare(int x, int y){
-    if(x > y) {
-        return 1;
-    } else if(x < y) {
-        return -1;
-    } else{
-        return 0;
-    }
-}
+int compare(int x, int y) return (x > y) ? 1 : ((x < y) ? -1 : 0);
 
 int main() {
-    int iter;
+    int iter, houses, num, dist, med;
     scanf("%d", &iter);
-    for(int x = 0; x < iter; x++){
-        int houses, num;
+    while(iter-- > 0){
         float sum = 0;
         vector<int> nums;
         scanf("%d", &houses);
@@ -28,15 +19,9 @@ int main() {
             nums.push_back(num);
         }
         sort(nums.begin(), nums.end(), compare);
-        int med = nums[houses/2];
-        int dist = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(med > nums[i]){
-                dist += med - nums[i];
-            } else{
-                dist += nums[i] - med;
-            }
-        }
+        med = nums[houses/2];
+        dist = 0;
+        for(int i = 0; i < nums.size(); i++) dist += (med > nums[i]) ? (med - nums[i]) : (nums[i] - med); 
         cout << dist << endl;
     }
     return 0;
