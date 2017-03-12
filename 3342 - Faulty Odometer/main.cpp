80ce0cd70840931a0946de7a8714730665b00ac4
Matouš Dzivjak <matoousmaster@gmail.com>
2017-03-12 18:06:35 +0100

    Update main.cpp

--- 3342 - Faulty Odometer/main.cpp
+++ 3342 - Faulty Odometer/main.cpp
@@ -10,32 +10,21 @@ using namespace std;
 long outPut(vector<char> number){
     long result = 0;
     int position = 0;
-    for(int i = number.size() - 1; i >= 0; i--){
-        result += (number[i]-48) * pow(9, position++);
-    }
+    for(int i = number.size() - 1; i >= 0; i--) result += (number[i]-48) * pow(9, position++);
     return result;
 }
 
-void vectos(vector<char> s){
-    for(int i = 0; i < s.size(); i++){
-        cout << s[i];
-    }
-}
-
 int main(){
-    long n = 0;
     char c;
-    vector<char> num;
-    vector<char> orig;
-    while(scanf("%c", &c)) {
-        if(c-48 == 0) break;
+    vector<char> num, orig;
+    while(scanf("%c", &c) && (c-48) != 0) {
         orig.push_back(c);
         num.push_back((c-48 > 4)?(c-1):c);
         while(scanf("%c", &c) && c != '\n'){
             orig.push_back(c);
             num.push_back((c-48 > 4)?(c-1):c);
         }
-        vectos(orig);
+        for(int i = 0; i < orig.size(); i++) cout << orig[i];
         cout << ": " << outPut(num) << endl;
         orig.clear();
         num.clear();
