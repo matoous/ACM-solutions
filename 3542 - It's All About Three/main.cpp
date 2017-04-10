98db684ff28a390a4786bd980be3ad2119673ffa
Matouš Dzivjak <matoousmaster@gmail.com>
2017-04-10 15:30:46 +0200

    Update main.cpp

--- 3542 - It's All About Three/main.cpp
+++ 3542 - It's All About Three/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
+
 bool fnc(int m){
     for(int i = 2;m != 1;i++){
-        if(m%i == 0){
-            if(i%10 != 3) return false;
-            while(m%i == 0) m/=i;
+        if(!m % i){
+            if(i % 10 != 3) return false;
+            while(m % i == 0) m /= i;
         }
     }
     return true;
