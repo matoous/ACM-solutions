e6a8f0ad4ce1a7d663d9b6609348752257fcd570
Matouš Dzivjak <matoousmaster@gmail.com>
2017-04-10 15:33:52 +0200

    Update main.cpp

--- 3539-The_Euclidian_Clock/main.cpp
+++ 3539-The_Euclidian_Clock/main.cpp
@@ -2,6 +2,7 @@
 
 #include <math.h>
 #include<cstdio>
+
 using namespace std;
 
 # define M_PI 3.14159265358979323846
@@ -14,7 +15,7 @@ int main(){
     for(int x = 0; x < t; x++){
         scanf("%lf %lf %lf %lf", &h, &m, &s, &u); //clock one
         scanf("%lf %lf %lf %lf", &h2, &m2, &s2, &u2); //clock two
-        scanf("%lf", &r); //r
+        scanf("%lf", &r);
         h2 -= h; m2 -= m; s2 -= s; u2 -= u;
         // convert angle to angle between 00:00:00.000 and some time
         if(u2 < 0){
@@ -30,10 +31,7 @@ int main(){
             h2--;
         }
         a = r*r*M_PI; // total radius
-        b = h2/12.0;
-        b += m2/(12.0*60.0);
-        b += s2/(12.0*60.0*60.0);
-        b += u2/(12.0*60.0*60.0*100.0);
+        b = h2/12.0 + m2/(12.0*60.0) + s2/(12.0*60.0*60.0) + u2/(12.0*60.0*60.0*100.0);
         printf("%d. %.3f\n",x+1, round(b*a*1000)/1000); // round to 3 digits
     }
     return 0;
